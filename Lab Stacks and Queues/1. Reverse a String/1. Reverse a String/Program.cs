559362fc0a91321106cbbf17eaef8f3e1689cefe
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-11 15:25:14 +0200

    Add files via upload

---
+++ Lab Stacks and Queues/1. Reverse a String/1. Reverse a String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            string str = Console.ReadLine();
+
+            Stack<string> stack = new Stack<string>();
+
+            for(int i=0; i<str.Length; i++)
+            {
+                stack.Push(str[i].ToString());
+            }
+
+            for(int i=stack.Count; i>0;  i--)
+            {
+                Console.Write($"{stack.Pop()}");
+            }
+        }
+    }
+}
