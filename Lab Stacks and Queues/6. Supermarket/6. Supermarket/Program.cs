13c11afbbd39de5588b3d55c7baf2e13cee658c0
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-11 18:10:23 +0200

    Add files via upload

---
+++ Lab Stacks and Queues/6. Supermarket/6. Supermarket/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> names = new Queue<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if((command!="Paid")&&(command!="End"))
+                {
+                    names.Enqueue(command);
+                }
+
+                if(command=="Paid")
+                {
+                    while (names.Count > 0)
+                    {
+                        Console.WriteLine(names.Dequeue());
+                    }
+                }
+
+                if(command=="End")
+                {
+                    Console.WriteLine($"{names.Count} people remaining.");
+                    break;
+                }
+            }
+
+        }
+    }
+}
