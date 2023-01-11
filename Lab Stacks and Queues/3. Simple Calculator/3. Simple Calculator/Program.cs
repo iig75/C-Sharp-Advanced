e6a9bc675d9f5a5009c469864e99331923020c6a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-11 17:19:01 +0200

    Add files via upload

---
+++ Lab Stacks and Queues/3. Simple Calculator/3. Simple Calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<string> expression = new Stack<string>(Console.ReadLine()
+                                                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                                 .ToArray().Reverse()
+                                                                 );
+
+            int sum = int.Parse(expression.Pop());
+            int i = 0;
+
+            while (expression.Count > 0)
+            {
+                if(i % 2 != 0)
+                {
+                    if(expression.Pop()=="+")
+                    {
+                        sum += int.Parse(expression.Pop());
+                    }
+                    else
+                    {
+                        sum -= int.Parse(expression.Pop());
+                    }
+                }
+
+                i++;                   
+            }
+
+            Console.WriteLine(sum);
+
+        }
+    }
+}
