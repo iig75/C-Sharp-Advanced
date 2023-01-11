802bc1f7d0eec8330f904a26fb3bb353cc62c58c
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-11 17:40:09 +0200

    Add files via upload

---
+++ Lab Stacks and Queues/4. Matching Brackets/4. Matching Brackets/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string expression = Console.ReadLine();
+
+            Stack<int> openBrackets = new Stack<int>();
+
+            for(int i=0; i<expression.Length; i++)
+            {
+
+                if (expression[i].ToString()=="(")
+                {
+                    openBrackets.Push(i);
+                }
+
+                if (expression[i].ToString()==")")
+                {
+                    string str=String.Empty;
+
+                    for(int h=openBrackets.Pop(); h<=i; h++)
+                    {
+                        str += expression[h];                        
+                    }
+
+                    Console.WriteLine($"{str}");
+                }
+            }
+
+        }
+    }
+}
