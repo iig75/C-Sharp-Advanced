3aa40927d988205e23b5a595b97fa4bdd35b42b3
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-11 19:54:26 +0200

    Add files via upload

---
+++ Lab Stacks and Queues/7. Hot Potato/7. Hot Potato/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Queue<string> kids = new Queue<string>(Console.ReadLine()
+                                                          .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                          .ToArray());
+
+            int count = int.Parse(Console.ReadLine());
+
+            int i = 1;
+
+            while(kids.Count>1)
+            {
+                if(i==count)
+                {
+                    Console.WriteLine($"Removed {kids.Dequeue()}");
+
+                    i = 1;
+                }
+                else
+                {
+                    kids.Enqueue(kids.Dequeue());
+                    i++;
+                }
+            }
+
+            Console.WriteLine($"Last is {kids.Peek()}");
+
+        }
+    }
+}
