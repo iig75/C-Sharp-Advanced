1c5a1522210e8782d6f3343859c9edba07eca5ba
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-13 14:06:01 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/1. Basic Stack Operations/1. Basic Stack Operations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1._Basic_Stack_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] input = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int N = input[0];
+            int S = input[1];
+            int X = input[2];
+
+            Stack<int> nums = new Stack<int>(Console.ReadLine()
+                                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                    .Select(int.Parse));
+
+            if(nums.Count>=S)
+            {
+                for(int i=1; i<=S; i++)
+                {
+                    nums.Pop();
+                }
+            }
+
+            if((nums.Contains(X)) && (nums.Count>0))
+            {
+                Console.WriteLine("true");
+            }
+
+            if ((!nums.Contains(X)) && (nums.Count > 0))
+            {
+                Console.WriteLine(nums.Min());
+            }
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine(0);
+            }
+
+        }
+    }
+}
---
+++ Exercise Stacks and Queues/2. Basic Queue Operations/2. Basic Queue Operations/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Basic_Queue_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                                 .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(int.Parse)
+                                 .ToArray();
+
+            int N = input[0];
+            int S = input[1];
+            int X = input[2];
+
+            Queue<int> nums = new Queue<int>(Console.ReadLine()
+                                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                    .Select(int.Parse));
+
+            if(nums.Count>=S)
+            {
+                for(int i=1; i<=S; i++)
+                {
+                    nums.Dequeue();
+                }
+            }
+
+            if ((nums.Count > 0) && (nums.Contains(X)))
+            {
+                Console.WriteLine("true");
+            }
+
+            if ((nums.Count > 0) && (!nums.Contains(X)))
+            {
+                Console.WriteLine($"{nums.Min()}");
+            }
+
+            if (nums.Count == 0)
+            {
+                Console.WriteLine($"0");
+            }
+
+        }
+    }
+}
