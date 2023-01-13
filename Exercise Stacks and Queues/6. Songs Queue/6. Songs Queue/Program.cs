3af5220b7148c1c147a6ca795b27b7d96a98efd7
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-13 15:56:31 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/6. Songs Queue/6. Songs Queue/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _6._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Queue<string> songs = new Queue<string>(Console.ReadLine()
+                                                           .Split(", ", StringSplitOptions.RemoveEmptyEntries));
+
+            while(true)
+            {
+
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "Play")
+                {
+                    if(songs.Count>0)
+                    {
+                        songs.Dequeue();
+                    }
+                }
+
+                if (command[0] == "Add")
+                {
+                    string song = command[1];
+
+                    for(int i=2; i<command.Length; i++)
+                    {
+                        song += " " + command[i];
+                    }
+
+                    if (songs.Contains(song))
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+                    else
+                    {
+                        songs.Enqueue(song);
+                    }
+                }
+
+                if (command[0] == "Show")
+                {
+                    Console.WriteLine(String.Join(", ", songs));
+                }
+
+                if (songs.Count == 0)
+                {
+                    Console.WriteLine("No more songs!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
