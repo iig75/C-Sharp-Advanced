553626f139fd27b77c81a0747e9ad4b526bc16ed
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-13 14:44:16 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/4. Fast Food/4. Fast Food/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<int> orders = new Queue<int>(Console.ReadLine()
+                                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                      .Select(int.Parse));
+
+            Console.WriteLine(orders.Max());
+
+            while(true)
+            {
+                if(n >= orders.Peek())
+                {
+                    n-=orders.Peek();
+                    orders.Dequeue();
+                }
+
+                if(orders.Count==0)
+                {
+                    Console.WriteLine("Orders complete");
+                    break;
+                }
+
+                if(n<orders.Peek())
+                {
+                    Console.WriteLine("Orders left: " + String.Join(" ", orders));
+                    break;
+                }
+
+            }
+
+        }
+    }
+}
