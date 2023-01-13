6dbc2aadc7c87541d7014a49152d3e9eddc051f9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-13 15:32:44 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/5. Fashion Boutique/5. Fashion Boutique/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                       .Select(int.Parse));
+
+            int rackSize = int.Parse(Console.ReadLine());
+
+            int racks = 1;
+            int sum = 0;
+
+            while(clothes.Count>0)
+            {
+                if(rackSize-sum>=clothes.Peek())
+                {
+                    sum += clothes.Pop();
+                }
+                else
+                {
+                    sum = 0;
+                    racks++;
+                }
+            }
+
+            Console.WriteLine(racks);
+
+        }
+    }
+}
