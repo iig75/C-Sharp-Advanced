b8fdfa0f35654687c00bb24b99ba1deb95e6a67c
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-14 14:29:26 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/8. Balanced Parentheses/8. Balanced Parentheses/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Balanced_Parentheses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string input = Console.ReadLine();
+
+            Stack<char> brackets= new Stack<char>();
+
+            bool succeed = true;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if ((input[i].ToString()=="{") || (input[i].ToString()=="[") || (input[i].ToString()=="("))
+                {
+                    brackets.Push(input[i]);
+                }
+
+                if (brackets.Count > 0)
+                {
+                    if ((input[i].ToString() == "}") && (brackets.Pop().ToString()!="{"))
+                    {
+                        Console.WriteLine("NO");
+                        succeed = false;
+                        break;
+                    }
+
+                    if ((input[i].ToString() == "]") && (brackets.Pop().ToString() != "["))
+                    {
+                        Console.WriteLine("NO");
+                        succeed = false;
+                        break;
+                    }
+
+                    if ((input[i].ToString() == ")") && (brackets.Pop().ToString() != "("))
+                    {
+                        Console.WriteLine("NO");
+                        succeed = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                    succeed = false;
+                    break;
+                }
+            }
+
+            if(succeed) 
+            {
+                Console.WriteLine("YES");
+            }
+
+        }
+    }
+}
