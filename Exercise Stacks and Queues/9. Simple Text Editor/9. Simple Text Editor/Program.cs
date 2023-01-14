1268852c9b84e6ccd914f4bdf153ac7525a20f3a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-14 15:26:10 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/9. Simple Text Editor/9. Simple Text Editor/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _9._Simple_Text_Editor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string str = String.Empty;
+
+            Stack<string> state = new Stack<string>();
+
+            state.Push(str);
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine()
+                                          .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "1")
+                {
+                    state.Push(str);
+
+                    str += command[1];
+                }
+
+                if (command[0] == "2")
+                {
+                    state.Push(str);
+
+                    str = str.Remove(str.Length - int.Parse(command[1]));
+                }
+
+                if (command[0] == "3")
+                {
+                    Console.WriteLine(str[int.Parse(command[1])-1]);
+                }
+
+                if (command[0] == "4")
+                {
+                    str = state.Pop();
+                }
+            }
+
+        }
+    }
+}
