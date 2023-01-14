8a211d2943365f4d6670a2d707d679b09801623d
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-14 11:54:47 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/7. Truck Tour/7. Truck Tour/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace _7._Truck_Tour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+
+            Queue <int[]> queue = new Queue<int[]>();
+
+            for(int i=0; i<n; i++)
+            {
+                queue.Enqueue(Console.ReadLine()
+                                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                     .Select(int.Parse)
+                                     .ToArray());
+            }
+
+            for(int i=0; i<n; i++)
+            {
+
+                bool complete = true;
+                int fuel = 0;
+                int distance = 0;
+
+                foreach (var item in queue)
+                {
+                    fuel += item[0];
+                    distance = item[1];
+
+                    if (fuel - distance < 0)
+                    {
+                        int[] temp = new int[2];
+
+                        queue.Enqueue(queue.Dequeue());
+                        
+                        complete = false;
+
+                        break;
+                    }
+                    else
+                    {
+                        fuel-= distance;
+                    }
+                }
+
+                if(complete)
+                {
+                    Console.WriteLine($"{i}");
+                    break;
+                }
+
+            }
+        }
+    }
+}
