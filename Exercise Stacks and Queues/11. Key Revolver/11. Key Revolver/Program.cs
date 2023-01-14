71ee4e2497570d25debf1741b44bf2ddf126fbad
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-14 19:50:40 +0200

    Add files via upload

---
+++ Exercise Stacks and Queues/11. Key Revolver/11. Key Revolver/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _11._Key_Revolver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int priceOfBullets = int.Parse(Console.ReadLine());
+
+            int gunBarrel = int.Parse(Console.ReadLine());
+
+            Stack<int> bullets = new Stack<int>(Console.ReadLine()
+                                                       .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                       .Select(int.Parse));
+
+            Queue<int> locks = new Queue<int>(Console.ReadLine()
+                                                     .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                                     .Select(int.Parse));
+
+            int intelligence = int.Parse(Console.ReadLine());
+
+            int usedBullets = 0;
+
+            while(true)
+            {
+
+                if (locks.Count == 0)
+                {
+                    Console.WriteLine($"{bullets.Count} bullets left. Earned ${(intelligence - (usedBullets * priceOfBullets))}");
+                    break;
+                }
+
+                if (bullets.Count==0)
+                {
+                    Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+                    break;
+                }
+
+                if (bullets.Pop() > locks.Peek())
+                {
+                    usedBullets++;
+                    Console.WriteLine("Ping!");
+                }
+                else
+                {
+                    usedBullets++;
+                    Console.WriteLine("Bang!");
+
+                    if (locks.Count > 0)
+                    {
+                        locks.Dequeue();
+                    }
+                }
+
+                if ((usedBullets % gunBarrel == 0) && (bullets.Count>0))
+                {
+                    Console.WriteLine("Reloading!");
+                }
+            }
+
+        }
+    }
+}
