01d84a781af5edfb0bda01c32c570f15160d12c6
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-15 14:13:08 +0200

    Add files via upload

---
+++ Lab Multidimensional Arrays/1. Sum Matrix Elements/1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _1._Sum_Matrix_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] size = Console.ReadLine()
+                                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray();
+
+            int row = size[0];
+            int col = size[1];
+
+            int[,] matrix = new int[row, col];
+
+            int sum = 0;
+
+            for (int i = 0; i < row; i++)
+            {
+                int[] rows = Console.ReadLine()
+                                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                    .Select(int.Parse)
+                                    .ToArray();
+
+                for (int j = 0; j < col; j++)
+                {
+                    matrix[i, j] = rows[j];
+                }
+            }
+
+            for(int i=0; i<row; i++)
+            {
+                for(int j=0; j<col; j++)
+                {
+                    sum+= matrix[i, j];
+                }
+            }
+
+            Console.WriteLine(row);
+            Console.WriteLine(col);
+            Console.WriteLine(sum);
+        }
+    }
+}
