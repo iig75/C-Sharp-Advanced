33fd64dd99910a294b6b5e39bccc02188499a109
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 16:45:43 +0200

    Add files via upload

---
+++ Lab Sets and Dictionaries Advanced/5. Cities by Continent and Country/5. Cities by Continent and Country/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace _5._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {           
+            Dictionary<string, Dictionary<string, List<string>>> countinents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                string[] input= Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string countinent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!countinents.ContainsKey(countinent))
+                {
+                    countinents.Add(countinent, new Dictionary<string, List<string>>());
+                    countinents[countinent].Add(country, new List<string>());
+                    countinents[countinent][country].Add(city);
+                }
+                else
+                {
+                    if (!countinents[countinent].ContainsKey(country))
+                    {
+                        countinents[countinent].Add(country, new List<string>());
+                        countinents[countinent][country].Add(city);
+                    }
+                    else
+                    {
+                        countinents[countinent][country].Add(city);
+                    }
+                }
+            }
+
+            foreach(var countinent in countinents)
+            {
+                Console.WriteLine($"{countinent.Key}:");
+
+                foreach(var country in countinent.Value)
+                {
+                    Console.Write($"  {country.Key} -> ");
+
+                    Console.Write(String.Join(", ", country.Value));
+
+                    Console.WriteLine();
+                }
+            }
+
+        }
+    }
+}
