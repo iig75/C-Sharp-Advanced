b657ef63b1b2204bd600c5a8a372ca8b9e4586d8
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 23:07:19 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/3. Periodic Table/3. Periodic Table/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> elements = new HashSet<string>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] str = Console.ReadLine()
+                                      .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                      .ToArray();
+
+                foreach(string st in str)
+                {
+                    elements.Add(st);
+                }
+            }
+
+            elements = elements.OrderBy(x => x).ToHashSet();
+
+            Console.WriteLine(String.Join(" ", elements));
+        }
+    }
+}
