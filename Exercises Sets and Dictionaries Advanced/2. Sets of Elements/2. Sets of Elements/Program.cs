0f90989c24af095093c6d7005083d565e30f0ea4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 22:59:51 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/2. Sets of Elements/2. Sets of Elements/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> set1= new HashSet<int>();
+            HashSet<int> set2= new HashSet<int>();
+
+            int[] nums = Console.ReadLine()
+                                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Select(int.Parse)
+                                .ToArray();
+
+            for(int i=0; i < nums[0]; i++)
+            {
+                set1.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < nums[1]; i++)
+            {
+                set2.Add(int.Parse(Console.ReadLine()));
+            }
+
+            set1.IntersectWith(set2);
+
+            Console.WriteLine(String.Join(" ", set1));
+        }
+    }
+}
