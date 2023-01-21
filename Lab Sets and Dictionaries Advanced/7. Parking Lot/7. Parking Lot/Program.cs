5b224378eb2c0caff2bb67f45fc7d0ae472fd9d7
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 17:09:52 +0200

    Add files via upload

---
+++ Lab Sets and Dictionaries Advanced/7. Parking Lot/7. Parking Lot/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _7._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> regNumber = new HashSet<string>();
+
+            while(true)
+            {
+                string[] command = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (command[0] == "END")
+                    break;
+
+                if (command[0]=="IN")
+                {
+                    regNumber.Add(command[1]);
+                }
+
+                if (command[0]=="OUT")
+                {
+                    regNumber.Remove(command[1]);
+                }
+            }
+
+            if(regNumber.Count>0)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, regNumber));
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
