cf7cd8e77b98356648c942ca9b02731bebaa1b76
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 18:40:37 +0200

    Add files via upload

---
+++ Lab Multidimensional Arrays/7. Pascal Triangle/7. Pascal Triangle/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace _7._Pascal_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            long[][] triangle = new long[n][];
+
+            for (int i = 0; i < triangle.Length; i++)
+            {
+                triangle[i] = new long[i+1];
+
+                for (int j=0; j < triangle[i].Length; j++)
+                {
+                    triangle[i][j] = 0;
+
+                    if ((isValid(i-1, j-1, triangle)) && (isValid(i - 1, j, triangle)))
+                    {
+                        triangle[i][j] = triangle[i-1][j-1] + triangle[i - 1][j];
+                    }
+                    else
+                    {
+                        triangle[i][j] = 1;
+                    }
+
+                }
+            }
+
+            PrintMatrix(triangle);
+        }
+
+        public static bool isValid(int i, int j, long[][] triangle)
+        {
+            return
+                i >= 0
+                && i < triangle.Length
+                && j >= 0
+                && j < triangle[i].Length;
+        }
+
+
+
+        private static void PrintMatrix(long[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write(matrix[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
