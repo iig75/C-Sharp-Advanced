d0a6d5e41fbf1942f7723bf0f39fe4162dd5f928
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 23:22:40 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/4. Even Times/4. Even Times/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _4._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, int> nums= new Dictionary<int, int>();
+
+            int n= int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                if(!nums.ContainsKey(x))
+                {
+                    nums.Add(x, 0);
+                }
+                nums[x]++;
+            }
+
+            Console.WriteLine(nums.Single(x => x.Value % 2 == 0).Key);
+        }
+    }
+}
