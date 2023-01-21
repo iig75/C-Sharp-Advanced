e1b1992e9d132fc89a22cf57ce53d3109680b2ef
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 23:45:01 +0200

    Add files via upload

---
+++ Exercises Sets and Dictionaries Advanced/5. Count Symbols/5. Count Symbols/OrderedDictionary.cs
@@ -0,0 +1,6 @@
+﻿namespace _5._Count_Symbols
+{
+    internal class OrderedDictionary<T1, T2>
+    {
+    }
+}
---
+++ Exercises Sets and Dictionaries Advanced/5. Count Symbols/5. Count Symbols/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace _5._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, int> letters= new Dictionary<char, int>();
+
+            string input = Console.ReadLine();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!letters.ContainsKey(input[i]))
+                {
+                    letters.Add(input[i], 0);
+                }
+
+                letters[input[i]]++;    
+            }
+
+            foreach (var letter in letters.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value))
+            {
+                Console.WriteLine($"{letter.Key}: {letter.Value} time/s");
+            }            
+        }
+    }
+}
