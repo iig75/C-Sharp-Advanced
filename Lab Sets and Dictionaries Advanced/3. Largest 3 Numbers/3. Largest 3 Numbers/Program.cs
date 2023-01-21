2c1e6b494c71182283539602d927da05f5ee667e
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-21 15:44:40 +0200

    Add files via upload

---
+++ Lab Sets and Dictionaries Advanced/3. Largest 3 Numbers/3. Largest 3 Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3._Largest_3_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            List<int> nums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            int count = 0;
+
+            nums = nums.OrderByDescending(x => x).ToList();
+
+            count = 0;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                Console.Write($"{nums[i]} ");
+
+                count++;
+
+                if (count == 3)
+                    break;
+            }
+        }
+    }
+}
