b3d57d8efce3e5b4811262ba59d5b1be8370e45f
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-26 23:31:30 +0200

    Add files via upload

---
+++ Lab Streams, Files and Directories/1. Odd Lines/1. Odd Lines/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace OddLines
+{
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            
+            var reader = new StreamReader(inputFilePath);
+
+            int i = 0;
+
+            using(reader)
+            {
+                
+
+                while(!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    i++;
+                }
+            }
+
+
+
+        }
+    }
+}
+
