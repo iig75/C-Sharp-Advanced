8dd2af3c8886acdd24681410a6d07297ca1140e9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-27 22:27:58 +0200

    Add files via upload

---
+++ Lab Streams, Files and Directories/4. Merge Files/4. Merge Files/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace MergeFiles
+{
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            var reader1 = new StreamReader(firstInputFilePath);
+            var reader2 = new StreamReader(secondInputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            using(writer)
+            {
+                string line1=String.Empty;
+                string line2=String.Empty;
+
+                while(true)
+                {
+                    if(!reader1.EndOfStream)
+                    {
+                        writer.WriteLine(reader1.ReadLine());
+                    }
+
+                    if(!reader2.EndOfStream)
+                    {
+                        writer.WriteLine(reader2.ReadLine());
+                    }
+
+                    if ((reader1.EndOfStream) && (reader2.EndOfStream))
+                    {
+                        break;
+                    }
+                }
+
+            }
+
+        }
+    }
+}
+
