356f6ee0b12b59d1b6e90066197b34c0cad8a5b2
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-27 19:04:54 +0200

    Add files via upload

---
+++ Lab Streams, Files and Directories/2. Line Numbers/2. Line Numbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            var reader = new StreamReader(inputFilePath);
+            var writer = new StreamWriter(outputFilePath);
+
+            using (reader)
+            {
+                using (writer)
+                {
+                    int i = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+
+                        line = $"{i}. " + line;
+
+                        writer.WriteLine(line);
+
+                        i++;
+                    }
+
+
+
+
+                }
+
+
+
+
+            }
+
+
+        }
+    }
+}
+
