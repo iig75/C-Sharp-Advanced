bbc39d6d13f98d384f9961eded5567898c1fe3a4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-28 18:36:58 +0200

    Add files via upload

---
+++ Lab Streams, Files and Directories/7. Folder Size/7. Folder Size/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace FolderSize
+{
+    public class FolderSize
+    {
+        static void Main()
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+            long size = GetFolderSize(folderPath);
+
+            File.WriteAllText(outputFilePath, $"{size / 1024} KB");
+        }
+
+
+
+        public static long GetFolderSize(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path);
+
+            long size = 0;
+
+            foreach (var filePath in filePaths)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                size += fileInfo.Length;
+            }
+
+            foreach(var dirPaths in Directory.GetDirectories(path))
+            {
+                size += GetFolderSize(dirPaths);
+            }    
+
+            return size;
+        }
+    }
+}
+
