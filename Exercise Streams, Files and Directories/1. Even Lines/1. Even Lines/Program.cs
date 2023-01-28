d9540b5562a5779c28b7628cab1133166ca2604d
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-28 19:38:38 +0200

    Add files via upload

---
+++ Exercise Streams, Files and Directories/1. Even Lines/1. Even Lines/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            var reader = new StreamReader(inputFilePath);
+
+            int i = 0;
+
+            while (!reader.EndOfStream)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(ProcessLines(reader.ReadLine()));
+                }
+                else
+                {
+                    reader.ReadLine();
+                }
+
+                i++;
+            }
+            
+        }
+
+        public static string ProcessLines(string str)
+        {
+            Random rand = new Random();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(str);
+
+            sb.Replace("-", "@");
+            sb.Replace(",", "@");
+            sb.Replace(".", "@");
+            sb.Replace("!", "@");
+            sb.Replace("?", "@");
+
+            List<string> line = sb.ToString().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            sb = new StringBuilder();
+
+            while (line.Count > 0)
+            {
+                int x = rand.Next(0, line.Count);
+                sb.Append(line[x]+" ");
+                line.RemoveAt(x);
+            }
+
+            return sb.ToString();
+        }
+
+
+
+    }
+}
