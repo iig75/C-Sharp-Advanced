3004a227ac83ca0748d5504a3d78edae6fd7b569
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-28 14:32:05 +0200

    Add files via upload

---
+++ Lab Streams, Files and Directories/5. Extract Special Bytes/5. Extract Special Bytes/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ExtractSpecialBytes
+{
+    public class ExtractSpecialBytes
+    {
+        static void Main()
+        {
+            string binaryFilePath = @"..\..\..\Files\example.png";
+            string bytesFilePath = @"..\..\..\Files\bytes.txt";
+            string outputPath = @"..\..\..\Files\output.bin";
+
+            ExtractBytesFromBinaryFile(binaryFilePath, bytesFilePath, outputPath);
+        }
+
+        public static void ExtractBytesFromBinaryFile(string binaryFilePath, string bytesFilePath, string outputPath)
+        {
+
+            var reader1 = new StreamReader(bytesFilePath);
+            var reader2 = new StreamReader(binaryFilePath);            
+            var writer = new StreamWriter(outputPath);
+
+            List<string> bytesList= new List<string>();
+
+
+
+            using(reader1)
+            {
+                while(!reader1.EndOfStream)
+                {
+                    bytesList.Add(reader1.ReadLine());
+                }
+            }
+
+
+
+            byte[] binFile = File.ReadAllBytes(binaryFilePath);
+
+            
+            StringBuilder sb = new StringBuilder();
+
+            foreach (byte b in binFile)
+            {
+                if (bytesList.Contains(b.ToString()))
+                {
+                    sb.Append(b.ToString()+" ");
+                }
+            }
+
+
+
+            using (writer)
+            {
+                writer.WriteLine(sb);
+            }
+
+
+        }
+    }
+}
+
