970f1e214b7dc6e151b459a6dc444a94280af889
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-29 10:52:59 +0200

    Add files via upload

---
+++ Exercise Streams, Files and Directories/1. Even Lines/1. Even Lines/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace EvenLines
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+            var reader = new StreamReader(inputFilePath);
+
+            int count = 0;
+
+            string line = String.Empty;
+            string reversedWords = String.Empty;
+
+            while (line != null)
+            {
+                line = reader.ReadLine();
+
+                if (count % 2 == 0)
+                {                    
+                    sb.AppendLine(reversedWords = ReversedWords(ReplaceSymbols(line)));
+                }
+
+                count++;
+            }
+
+            return sb.ToString();
+        }
+
+
+
+        public static string ReplaceSymbols(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(str);
+
+            char[] symbols = new char[] { '-', ',', '.', '!', '?' };
+
+            foreach (char c in symbols)
+            {
+                sb.Replace((char)c, '@');
+            }
+
+            return sb.ToString();
+        }
+
+
+
+        public static string ReversedWords(string str)
+        {
+            string[] array = str.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                                .Reverse()
+                                .ToArray();
+
+            return String.Join(" ", array);
+        }
+    }
+}
