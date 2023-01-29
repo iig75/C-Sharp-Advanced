74ced3086843a69804379390c6e77db8c19c911f
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-29 17:54:36 +0200

    Add files via upload

---
+++ Exercise Streams, Files and Directories/5. Copy Directory/5. Copy Directory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace CopyDirectory
+{
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath = @"C:\Users\iig\Desktop\Test\";         //@$"{Console.ReadLine()}";
+            string outputPath = @"C:\Users\iig\Desktop\Test - Copy\"; //@$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if(Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+            }
+
+            Directory.CreateDirectory(outputPath);
+
+            string[] files = Directory.GetFiles(inputPath); 
+
+            foreach(string file in files)
+            {
+                string filename = Path.GetFileName(file);
+                
+                string destination = Path.Combine(outputPath, filename);
+
+                File.Copy(file, destination);
+            }
+        }
+
+    }
+}
+
