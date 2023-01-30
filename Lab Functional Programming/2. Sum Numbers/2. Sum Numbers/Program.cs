2a697b65539578cec036bd7064eb2af28a50453d
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-30 12:04:33 +0200

    Add files via upload

---
+++ Lab Functional Programming/2. Sum Numbers/2. Sum Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+int[] nums = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+Console.WriteLine(nums.Count());
+Console.WriteLine(nums.Sum());
+
+
+
+
+
+
