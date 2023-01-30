6e2b480fe21cd80a31d2ad4921a17ba02bc4025a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-30 15:44:00 +0200

    Add files via upload

---
+++ Lab Functional Programming/4. Add VAT/4. Add VAT/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+double[] nums = Console.ReadLine()
+                       .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                       .Select(s => (double.Parse(s) *1.2))
+                       .ToArray();
+
+foreach (double num in nums)
+{
+    Console.WriteLine($"{num:f2}");
+}
+
+
+
