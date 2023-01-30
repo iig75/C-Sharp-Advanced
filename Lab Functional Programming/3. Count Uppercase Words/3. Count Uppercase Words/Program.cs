2756ec3e8fed29e1bf0eae7db5a235fc837a3a19
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-30 15:08:19 +0200

    Add files via upload

---
+++ Lab Functional Programming/3. Count Uppercase Words/3. Count Uppercase Words/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+string[] items = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Where(x => x[0] == char.ToUpper(x[0]) && char.IsLetter(x[0]))
+                        .ToArray();
+
+foreach(var item in items)
+{
+    Console.WriteLine($"{item}");
+}
