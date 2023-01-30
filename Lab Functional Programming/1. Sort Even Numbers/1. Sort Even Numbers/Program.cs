dc873baf38f709450cfa06efa9e0fcd790de41f4
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-30 11:42:17 +0200

    Add files via upload

---
+++ Lab Functional Programming/1. Sort Even Numbers/1. Sort Even Numbers/Program.cs
@@ -0,0 +1,10 @@
+﻿
+
+
+Console.WriteLine(String.Join(", ", Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                                                      .Select(int.Parse)
+                                                      .Where(x => x % 2 ==0)
+                                                      .OrderBy(x => x)
+                                                      .ToArray()));
+
+
