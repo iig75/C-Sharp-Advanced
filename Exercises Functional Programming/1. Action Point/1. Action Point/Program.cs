9ba651b4572153b84a0010e4d3e7d7e34c6d49d9
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-31 18:01:08 +0200

    Add files via upload

---
+++ Exercises Functional Programming/1. Action Point/1. Action Point/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Action<string> print = name => Console.WriteLine(name);
+
+string[] users = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string user in users)
+{
+    print(user);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
