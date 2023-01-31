8957c3315bfe7117b64f13426d58acdcda91cd8c
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-01-31 22:24:38 +0200

    Add files via upload

---
+++ Exercises Functional Programming/2. Knights of Honor/2. Knights of Honor/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+
+Action<string, string> print = (name, title) => Console.WriteLine($"{name} {title}");
+
+string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string name in names)
+{
+    print("Sir", name);
+}
+
+
+
+
+
+
+
