42853feff265ea34af996eac70cd32811aba03a5
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-01 19:48:48 +0200

    Add files via upload

---
+++ Exercises Functional Programming/7. Predicate for Names/7. Predicate for Names/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Predicate<int[]> even = z => (z[0] <= z[1]);
+
+Action<string> print = name => Console.WriteLine(name);
+
+Action<string[], int> counter = (names, n) =>
+{
+    foreach (var name in names)
+    {
+        int[] x = new int[2] { name.Length, n};
+
+        if (even(x))
+        {
+            print(name);
+        }
+    }
+};
+
+
+int n = int.Parse(Console.ReadLine());
+
+string[] names = Console.ReadLine()
+                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+counter(names, n);
