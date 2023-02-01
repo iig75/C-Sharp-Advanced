44590783a223da9f27eaefa986a13a15df2e13cb
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-01 18:56:07 +0200

    Add files via upload

---
+++ Exercises Functional Programming/5. Applied Arithmetics/5. Applied Arithmetics/Program.cs
@@ -0,0 +1,57 @@
+﻿
+Action<int[]> add = nums =>
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] += 1;
+    }
+};
+
+Action<int[]> multiply = nums =>
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] *= 2;
+    }
+};
+
+Action<int[]> subtract = nums =>
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        nums[i] -= 1;
+    }
+};
+
+Action<int[]> print = nums => Console.WriteLine(String.Join(" ", nums));
+
+
+
+int[] nums = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+string command = String.Empty;
+
+while((command = Console.ReadLine()) != "end")
+{
+    switch(command)
+    {
+        case "add":
+            add(nums);
+            break;
+
+        case "multiply":
+            multiply(nums);
+            break;
+
+        case "subtract":
+            subtract(nums);
+            break;
+
+        case "print":
+            print(nums);
+            break;
+    }
+}
