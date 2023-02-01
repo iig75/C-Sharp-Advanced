cdf233d8c33651ff629bc530399465876ed78493
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-01 22:50:58 +0200

    Add files via upload

---
+++ Exercises Functional Programming/8. List of Predicates/8. List of Predicates/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Predicate<int[]> checkDivision = z => (z[0] % z[1] == 0);
+
+Action<List<int>> print = prn => Console.WriteLine(String.Join(" ", prn));
+
+int n = int.Parse(Console.ReadLine());
+
+int[] array = Console.ReadLine()
+                            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                            .Select(int.Parse)
+                            .ToArray();
+
+List<int> results = new List<int>(0); 
+
+for(int i=1; i<=n; i++)
+{
+    bool isValid = true;
+    
+    for(int j=0; j<array.Length; j++)
+    {
+        int[] nums = new int[2] {i, array[j] };
+        
+        if(!checkDivision(nums))
+        {
+            isValid= false;
+            break;
+        }
+    }
+
+    if(isValid)
+    {
+        results.Add(i);
+    }
+}
+
+print(results);
