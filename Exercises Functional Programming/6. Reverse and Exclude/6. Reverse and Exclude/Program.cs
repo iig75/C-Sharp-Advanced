0837792b8fd3a659b8c4a881d4da8ca1621263a3
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-01 19:17:09 +0200

    Add files via upload

---
+++ Exercises Functional Programming/6. Reverse and Exclude/6. Reverse and Exclude/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+Action<List<int>> print = nums => Console.WriteLine(String.Join(" ", nums)); 
+
+
+
+Func<List<int>, int, string> divider = (nums, z) =>
+{
+    for(int i=0; i<nums.Count; i++)
+    {
+        if (nums[i] % z == 0)
+        {
+            nums.RemoveAt(i);
+            i--;
+        }
+    }
+
+    nums.Reverse();
+
+    return null;
+};
+
+
+
+List<int> nums = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+
+int n = int.Parse(Console.ReadLine());
+
+divider(nums, n);
+print(nums);
+
+
+
+
+
+
+
