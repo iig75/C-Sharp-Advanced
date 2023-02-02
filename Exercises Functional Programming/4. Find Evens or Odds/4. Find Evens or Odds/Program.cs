4b9cfabbd0845167cc64f6d325c12ce1d1794ff7
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-02 14:29:37 +0200

    Add files via upload

---
+++ Exercises Functional Programming/4. Find Evens or Odds/4. Find Evens or Odds/Program.cs
@@ -0,0 +1,42 @@
+﻿
+List<int> result = new List<int>();
+
+Predicate<int> match = x => x % 2 == 0;
+
+Action<List<int>> print = n => Console.WriteLine(String.Join(" ", n));
+
+while (true)
+{
+    int[] range = Console.ReadLine()
+                         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                         .Select(int.Parse)
+                         .ToArray();
+
+    string command = Console.ReadLine();    
+
+    if (command == "odd")
+    {
+        for (int i = range[0]; i <= range[1]; i++)
+        {
+            if(!match(i))
+            {
+                result.Add(i);
+            }
+        }
+    }
+
+    if (command == "even")
+    {
+        for (int i = range[0]; i <= range[1]; i++)
+        {
+            if (match(i))
+            {
+                result.Add(i);
+            }
+        }
+    }
+
+    break;
+}
+
+print(result);
