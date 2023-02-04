c95442a9e96a57792a373f5199d488306db12cab
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-04 17:24:19 +0200

    Add files via upload

---
+++ Exercises Functional Programming/11. TriFunction/11. TriFunction/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+
+Func<string, int, bool> CheckNameSum = (name, limit) => name.Sum(ch => ch) >= limit;
+
+
+
+Func<string[], int, Func<string, int, bool>, string> CheckNames = (names, limit, match) => names.FirstOrDefault(name => match(name, limit));
+
+
+
+int n = int.Parse(Console.ReadLine());
+
+string[] persons = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+
+Console.WriteLine(CheckNames(persons, n, CheckNameSum));
+
+
+/*
+foreach(string person in persons)
+{
+    int sum = 0;
+
+    for(int i=0; i<person.Length; i++)
+    {
+        sum += person[i];
+    }
+
+    if(sum >= n)
+    {
+        Console.WriteLine(person);
+        break;
+    }
+}
+*/
+
+
+
+
