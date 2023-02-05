d8991c16bd2fab25803962abdc1e2551572e1fd5
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-05 15:38:52 +0200

    Add files via upload

---
+++ Exercise Defining Classes/1. Define a Class Person/1. Define a Class Person/Person.cs
@@ -0,0 +1,25 @@
+﻿
+namespace DefiningClasses
+{
+
+
+    public class Person
+    {
+
+        private string name;
+        private int age;
+
+
+
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+
+
+
+
+
+
+}
---
+++ Exercise Defining Classes/1. Define a Class Person/1. Define a Class Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿
+
+
+namespace DefiningClasses
+{
+
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            Person person1 = new Person();
+            person1.Name = "Peter";
+            person1.Age = 20;
+
+            Person person2 = new Person();
+            person2.Name = "George";
+            person2.Age = 18;
+
+            Person person3 = new Person();
+            person1.Name = "Jose";
+            person1.Age = 43;
+        } 
+    
+
+
+    }
+
+}
+
+
