b422fb18c1748e863194779a85deef41376c4155
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-05 18:40:49 +0200

    Add files via upload

---
+++ Exercise Defining Classes/3. Oldest Family Member/3. Oldest Family Member/Family.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses
+{
+
+    public class Family
+    {
+        private List<Person> member;
+
+        public Family()
+        {
+            this.member = new List<Person>();
+        }
+
+        public List<Person> Member { get; set; }
+
+        public void AddMember(Person member)
+        {
+            this.member.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.member.MaxBy(x => x.Age);
+        }
+    }
+
+}
