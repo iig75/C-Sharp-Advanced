45913b405ab7db0662ee3f15f820973dc339e75b
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-05 18:43:49 +0200

    Add files via upload

---
+++ Exercise Defining Classes/2. Creating Constructors/2. Creating Constructors/StartUp.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+namespace DefiningClasses
+{
+
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            Person person1 = new Person();
+            Person person2 = new Person(18);
+            Person person3 = new Person("Jose", 43);
+        }
+    }
+}
+
+
