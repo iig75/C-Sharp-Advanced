ad113eb1a152fd16e9c883135a2a350c479e0260
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-05 15:37:26 +0200

    Add files via upload

---
+++ Lab Defining Classes/4. Car Engine and Tires/4. Car Engine and Tires/Car.cs
@@ -0,0 +1,78 @@
+﻿namespace CarManufacturer
+{
+
+    public class Car
+    {
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+            : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) :
+            this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tires) :
+            this(make, model, year, fuelQuantity, fuelConsumption)
+        {
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+
+
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double FuelQuantity { get; set; }
+        public double FuelConsumption { get; set; }
+        public Engine Engine { get; set; }
+        public Tire[] Tires { get; set; }
+
+
+
+        public void Drive(double distance)
+        {
+            if ((this.fuelQuantity - (this.fuelConsumption * distance)) > 0)
+            {
+                this.fuelQuantity -= distance * this.fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\r\nModel: {this.Model}\r\nYear: {this.Year}\r\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+
+
+
+}
---
+++ Lab Defining Classes/4. Car Engine and Tires/4. Car Engine and Tires/Tire.cs
@@ -0,0 +1,23 @@
+﻿namespace CarManufacturer
+{
+
+    public class Tire
+    {
+        private int year;
+        private int pressure;
+
+        public Tire(int year, double pressure)
+        {
+            this.Year = year;
+            this.Pressure = pressure;
+        }
+
+        public int Year {get; set;}
+        public double Pressure { get; set;}
+    }
+
+
+
+
+
+}
