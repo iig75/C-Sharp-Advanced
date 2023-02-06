e190ac9478d7a22c742933f70c778cf82853a5d1
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-06 16:17:06 +0200

    Add files via upload

---
+++ Exercise Defining Classes/4. Opinion Poll/4. Opinion Poll/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace DefiningClasses
+{
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }    
+    }
+
+}
---
+++ Exercise Defining Classes/4. Opinion Poll/4. Opinion Poll/StartUp.cs
@@ -0,0 +1,36 @@
+﻿
+namespace DefiningClasses
+{
+
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            List<Person> persons = new List<Person>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                Person person = new Person(input[0], int.Parse(input[1]));
+
+                persons.Add(person);
+            }
+
+
+
+            foreach(var person in persons.Where(x=>x.Age > 30).OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+
+        }
+    }
+}
+
+
+
+
+
