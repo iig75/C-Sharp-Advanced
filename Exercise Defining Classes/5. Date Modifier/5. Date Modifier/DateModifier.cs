40ec2ae6d1e31a324803fa1ce849a129f5d190f5
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-06 19:06:37 +0200

    Add files via upload

---
+++ Exercise Defining Classes/5. Date Modifier/5. Date Modifier/DateModifier.cs
@@ -0,0 +1,20 @@
+﻿namespace DateAndTime
+{
+    public class DateModifier
+    {
+
+        public DateModifier(DateTime date1, DateTime date2)
+        {
+            Date1 = date1;
+            Date2 = date2;
+        }
+
+        public DateTime Date1 { get; set; }
+        public DateTime Date2 { get; set; }
+
+        public int CalculateDays()
+        {
+            return Math.Abs((this.Date1.Date - this.Date2.Date).Days);
+        }
+    }
+}
---
+++ Exercise Defining Classes/5. Date Modifier/5. Date Modifier/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace DateAndTime
+{
+
+    public class StartUp
+    {
+
+        static void Main()
+        {
+            DateTime input1 = Convert.ToDateTime(Console.ReadLine());
+
+            DateTime input2 = Convert.ToDateTime(Console.ReadLine());
+
+            DateModifier calc1 = new DateModifier(input1, input2);
+
+            Console.WriteLine(calc1.CalculateDays());
+        }
+    }
+}
