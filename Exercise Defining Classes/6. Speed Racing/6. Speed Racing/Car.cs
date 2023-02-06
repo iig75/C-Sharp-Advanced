d8cb14083ff533b73881fd95b09a49fcbe414b0c
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-06 20:39:25 +0200

    Add files via upload

---
+++ Exercise Defining Classes/6. Speed Racing/6. Speed Racing/Car.cs
@@ -0,0 +1,38 @@
+﻿namespace SpeedRacing
+{
+
+    public class Car
+    {
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer, double tarvelledDistance)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = tarvelledDistance;
+        }
+
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+        
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
+
+
+
+        public void Drive(double distance)
+        {
+            if(this.FuelAmount >= this.FuelConsumptionPerKilometer*distance)
+            {
+                this.FuelAmount -= (this.FuelConsumptionPerKilometer * distance);
+                this.TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine($"Insufficient fuel for the drive");
+            }
+        }
+
+    }
+}
---
+++ Exercise Defining Classes/6. Speed Racing/6. Speed Racing/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+
+namespace SpeedRacing
+{
+
+    public class StartUp
+    {
+
+        static void Main()
+        {         
+            List<Car> cars = new List<Car>();
+
+            int n= int.Parse(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                string[] command = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = new Car(command[0], double.Parse(command[1]), double.Parse(command[2]), 0);
+
+                cars.Add(car);
+            }
+
+
+
+            string input = String.Empty;
+
+            while((input = Console.ReadLine()) != "End")
+            {
+                string[] command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach(Car car in cars)
+                {
+                    if(car.Model == command[1])
+                    {
+                        car.Drive(double.Parse(command[2]));
+                        break;
+                    }
+                }
+            }
+
+
+
+            foreach(Car car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        
+        } 
+    }
+}
