af5a38704b52f47eb53c8f5dec85c5bb7dbb1286
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-08 23:05:15 +0200

    Add files via upload

---
+++ Exercise Defining Classes/7. Raw Data/7. Raw Data/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection;
+
+namespace RowData
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Car> cars = new List<Car>();
+
+            int count = int.Parse(Console.ReadLine());
+
+            for(int i=0; i<count; i++)
+            {
+                string[] input = Console.ReadLine()
+                                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Car car = new Car(input[0],
+                                  int.Parse(input[1]),
+                                  int.Parse(input[2]),
+                                  int.Parse(input[3]),
+                                  input[4],
+                                  double.Parse(input[5]),
+                                  int.Parse(input[6]),
+                                  double.Parse(input[7]),
+                                  int.Parse(input[8]),
+                                  double.Parse(input[9]),
+                                  int.Parse(input[10]),
+                                  double.Parse(input[11]),
+                                  int.Parse(input[12])
+                                 );
+
+                cars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+
+            string[] filter;
+
+            if(command == "fragile")
+            {
+                filter = cars.Where(x=>x.Cargo.Type == "fragile" && x.Tyres.Any(z=>z.Pressure < 1)).Select(y=>y.Model).ToArray();
+            }
+            else
+            {
+                filter = cars.Where(x => x.Cargo.Type == "flammable" && x.Engine.Power > 250).Select(y => y.Model).ToArray();
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, filter));
+        }
+    }
+}
---
+++ Exercise Defining Classes/7. Raw Data/7. Raw Data/Tyre.cs
@@ -0,0 +1,15 @@
+﻿namespace RowData
+{
+    public class Tyre
+    {
+        public Tyre(double pressure, int age)
+        {
+            Pressure = pressure;
+            Age = age;
+        }
+
+        public double Pressure { get; set; }
+        public int Age { get; set; }
+
+    }
+}
