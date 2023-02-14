fc507a24b04d153a6279a8a4952e48779411078a
Iliyan Gospodinov <61354986+iig75@users.noreply.github.com>
2023-02-14 22:58:11 +0200

    Add files via upload

---
+++ Exercise Defining Classes/9. Pokemon Trainer/9. Pokemon Trainer/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace PokemonTrainer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            List<Trainer> trainers = new List<Trainer>();
+
+            string command = String.Empty;
+
+            while ((command = Console.ReadLine()) != "Tournament")
+            {
+
+                string[] tokens = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Trainer trainer = trainers.FirstOrDefault(x => x.Name == tokens[0]);
+
+                if(trainer == null)
+                {
+                    trainer = new Trainer(tokens[0]);
+
+                    Pokemon pokemon = new Pokemon(tokens[1], tokens[2], int.Parse(tokens[3]));
+
+                    trainer.Pokemons.Add(pokemon);
+
+                    trainers.Add(trainer);
+                }
+                else
+                {
+                    Pokemon pokemon = new Pokemon(tokens[1], tokens[2], int.Parse(tokens[3]));
+
+                    trainer.Pokemons.Add(pokemon);
+                }
+
+            }
+
+
+
+            while ((command = Console.ReadLine()) != "End")
+            {
+                foreach (Trainer trainer in trainers)
+                {
+                    trainer.Calculate(command);
+                }
+            }
+
+
+
+            foreach (Trainer trainer in trainers.OrderByDescending(x=>x.Badges))
+            {
+                Console.WriteLine(trainer.ToString());
+            }
+
+
+
+        }
+
+    }
+}
---
+++ Exercise Defining Classes/9. Pokemon Trainer/9. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,50 @@
+﻿using System.Data;
+
+namespace PokemonTrainer
+{
+    public class Trainer
+    {
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            Pokemons = new List<Pokemon>();
+        }
+
+        public string Name { get; set; }
+
+        public int Badges { get; set; }
+
+        public List<Pokemon> Pokemons { get; set; }
+
+
+        public void Calculate(string command)
+        {           
+            if (Pokemons.Any(x=>x.Element == command))
+            {
+                Badges++;
+            }
+            else
+            {
+                for(int i=0; i<Pokemons.Count; i++)
+                {
+                    Pokemon currentPokemon = Pokemons[i];
+                    
+                    currentPokemon.Health -= 10;
+
+                    if (currentPokemon.Health <= 0)
+                    {
+                        Pokemons.Remove(currentPokemon);
+                    }
+                }
+            }
+
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Badges} {Pokemons.Count()}";
+        }
+    }
+
+}
